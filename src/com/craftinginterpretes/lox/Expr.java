da3426bfaaf03da9dbba04b767f2c3683c2e6d7b
SamuelLindao <lacerdasamuel26@gmail.com>
2025-04-22 13:35:43 -0300

    Parser Start

---
+++ src/com/craftinginterpretes/lox/Expr.java
@@ -0,0 +1,15 @@
+﻿package com.craftinginterpretes.lox;
+
+abstract class Expr {
+    static class Binary extends Expr {
+        Binary(Expr left, Token operator, Expr right) {
+            this.left = left;
+            this.operator = operator;
+            this.right = right;
+        }
+        final Expr left;
+        final Token operator;
+        final Expr right;
+    }
+// Other expressions...
+}
